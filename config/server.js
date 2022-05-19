3d869ade00e6ac5017b0e510f2823af1e263b0c6
root <root@Strapi-VM.df3gv1imcuhedl3vcbftbeaqdh.bx.internal.cloudapp.net>
2022-05-19 12:16:46 +0000

    some changes

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  ssl: false,
+  url:"http://20.51.193.70",
   app: {
     keys: env.array('APP_KEYS'),
   },
