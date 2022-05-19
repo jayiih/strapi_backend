4dc7292dccbd334be171538a27c449d5f4df890a
pc12 <bhavin.iihglobal@gmail.com>
2022-05-19 14:47:58 +0530

    some changes db related

--- config/database.js
+++ config/database.js
@@ -3,12 +3,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "Jay@2001"),
-      schema: env("DATABASE_SCHEMA", "public"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA"),
     },
   },
 });
