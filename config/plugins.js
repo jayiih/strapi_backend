32a4006c85c60bf52a6717c5671c97795bc8c848
pc12 <bhavin.iihglobal@gmail.com>
2022-12-06 16:26:12 +0530

    fix mail on stripi

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,15 @@
 module.exports = ({ env }) => ({
   // ...
   email: {
-    provider: env('MAIL_HOST'),
-    providerOptions: {
-      apiKey: env('MAIL_PASSWORD'),
-    },
-    settings: {
-      defaultFrom: env('MAIL_FROM_ADDRESS'),
-      defaultReplyTo: env('MAIL_REPLY_TO_ADDRESS'),
+    config: {
+      provider: env('MAIL_HOST'),
+      providerOptions: {
+        apiKey: env('MAIL_PASSWORD'),
+      },
+      settings: {
+        defaultFrom: env('MAIL_FROM_ADDRESS'),
+        defaultReplyTo: env('MAIL_REPLY_TO_ADDRESS'),
+      },
     },
   },
   // ...
